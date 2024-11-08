6f7a89bf6db1cfedbe68e01618385eb262230b8b
Taylan <taylancankose@gmail.com>
2024-11-08 14:09:04 +0300

    runtime bugfix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
